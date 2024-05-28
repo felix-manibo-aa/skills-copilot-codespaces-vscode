0b50fb869a3253b6bd6f2c6f83174f7c460d7c74
Felix Manibo <155397489+felix-manibo-aa@users.noreply.github.com>
2024-05-28 10:35:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,28 @@
-// create a web server
+// Create web server with express
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
-  res.end(
-    JSON.stringify({
-      status: 'success',
-      message: 'You have reached the comment endpoint',
-    })
-  );
+// Create a new comment
+app.post('/comments', (req, res) => {
+  res.send('Creating a new comment');
 });
 
-server.listen(8000, () => {
-  console.log('Server is listening on port 8000');
+// Get a comment by id
+app.get('/comments/:id', (req, res) => {
+  res.send(`Getting comment with id ${req.params.id}`);
+});
+
+// Update a comment by id
+app.put('/comments/:id', (req, res) => {
+  res.send(`Updating comment with id ${req.params.id}`);
+});
+
+// Delete a comment by id
+app.delete('/comments/:id', (req, res) => {
+  res.send(`Deleting comment with id ${req.params.id}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
